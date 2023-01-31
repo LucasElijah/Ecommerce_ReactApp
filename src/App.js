28be3b0365b13458a5f3ddfb225ba3ebb011b2e6
LucasElijah <elijahlucas96@gmail.com>
2023-01-31 15:59:46 -0800

    idk

--- src/App.js
+++ src/App.js
@@ -2,21 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import path from "node:path/win32";
+import { element } from "prop-types";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Home</div>,
-  },
-  {
-    path: "/products/:id",
-    element: <div>Category</div>,
-  },
-  {
-    path: "/product/:id",
-    element: <div>Product</div>,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/Products/:id",
+        element: <Products />
+      },
+      {
+        path: "/Products/:id",
+        element: <Products />
+      },
+    ]
   },
 ]);
 
