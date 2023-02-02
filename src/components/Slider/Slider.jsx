5d6a6c4108749a878d138faf85a163ac82cb424c
LucasElijah <elijahlucas96@gmail.com>
2023-02-01 21:30:36 -0800

    slider component

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import { EastOutlined, WestOutlined } from "@mui/icons-material";
+import React, { useState } from "react";
+import "./Slider.scss";
 
 const Slider = () => {
-  return (
-    <div className='slider'>
-      <div className="container"></div>
-    </div>
-  )
-}
 
-export default Slider
+	const [currentSlide, setCurrentSlide] = useState(0);
+
+	const data = [
+		"https://images.pexels.com/photos/1884584/pexels-photo-1884584.jpeg?auto=compress&cs=tinysrgb&w=1600",
+		"https://images.pexels.com/photos/5119211/pexels-photo-5119211.jpeg?auto=compress&cs=tinysrgb&w=1600",
+		"https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+	];
+
+	const prevSlide = () => {
+		setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
+	};
+	const nextSlide = () => {
+		setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
+	};
+
+	return (
+		<div className="slider">
+			<div
+				className="container"
+				style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+			>
+				<img src={data[0]} alt="" />
+				<img src={data[1]} alt="" />
+				<img src={data[2]} alt="" />
+			</div>
+			<div className="icons">
+				<div className="icon" onClick={prevSlide}>
+					<WestOutlined />
+				</div>
+				<div className="icon" onClick={nextSlide}>
+					<EastOutlined />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Slider;
