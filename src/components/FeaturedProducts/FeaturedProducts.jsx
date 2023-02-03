42619f9cbdc328bfc808bcf1fba8891fcb153671
LucasElijah <elijahlucas96@gmail.com>
2023-02-02 16:48:25 -0800

    card

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -51,14 +51,3 @@ const FeaturedProducts = ({type}) => {
 }
 
 export default FeaturedProducts
-
-/* 
-https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinys%20rgb&w=1600
-https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinys%20rgb&w=1600
-
-https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto-compress&cs=tinys%20rgb&w=1600
-
-https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto-compress&cs=tinysrgb&w=1600
-
-https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto-compress&cs=tinys rgb&w=1600
-*/
