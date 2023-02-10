0acf9bc3d83a58fce5b1d688f9dd42f29053f0b0
LucasElijah <elijahlucas96@gmail.com>
2023-02-09 19:48:44 -0800

    cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { DeleteOutlined } from "@mui/icons-material";
+import React from "react";
 
 const Cart = () => {
-  return (
-    <div className="cart">
-      CART
-    </div>
-  )
-}
+	const date = [
+		{
+			id: 1,
+			img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinys%20rgb&w=1600",
+			img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinys%20rgb&w=1600",
+			title: "Long Sleeve",
+			isNew: true,
+			oldPrice: 34,
+			price: 27,
+		},
+		{
+			id: 2,
+			img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto-compress&cs=tinys%20rgb&w=1600",
+			title: "Shirt",
+			isNew: true,
+			oldPrice: 20,
+			price: 16,
+		},
+	];
 
-export default Cart
+	return (
+    <div className="cart">
+      <h1>Products in your cart</h1>
+      {data?.map(item=>(
+        <div className="item" key={item.id}>
+          <img src={item.img} alt="" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.desc.substring(0, 100)}</p>
+            <div className="price">1 x ${item.price}</div>
+          </div>
+          <DeleteOutlined />
+        </div>
+      ))}
+      <div className="total">
+        <span>SUBTOTAL</span>
+        <span>$69</span>
+      </div>
+    </div> 
+  );
+};
+
+export default Cart;
