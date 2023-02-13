1c6af6650dd706697c481eb738a2938a54637752
LucasElijah <elijahlucas96@gmail.com>
2023-02-13 14:04:30 -0800

    api code

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,76 +1,52 @@
-import React, { useState } from 'react'
-import "./FeaturedProducts.scss"
-import Card from '../Card/Card'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./FeaturedProducts.scss";
+import Card from "../Card/Card";
+import axios from "axios";
 
+const FeaturedProducts = ({ type }) => {
 
-const FeaturedProducts = ({type}) => {
+	const [data, setData] = useState([]);
 
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinys%20rgb&w=1600",
-      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinys%20rgb&w=1600",
-      title: "Long Sleeve",
-      isNew: true,
-      oldPrice: 34,
-      price: 27,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto-compress&cs=tinys%20rgb&w=1600",
-      title: "Shirt",
-      isNew: true,
-      oldPrice: 20,
-      price: 16,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto-compress&cs=tinysrgb&w=1600",
-      title: "Apparel",
-      oldPrice: 19,
-      price: 13,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto-compress&cs=tinys rgb&w=1600",
-      title: "Misc",
-      oldPrice: 19,
-      price: 10,
-    },
-  ];
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await axios.get(
+					process.env.REACT_APP_API_URL + "/products?populate=*",
+					{
+						headers: {
+							Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
+						},
+					}
+				);
+        setData(res.data.data)
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		fetchData();
+	}, []);
 
-  const [products, setProducts] = useState([]);
+  
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await axios.get(process.env.REACT_APP_API_URL+"/products", {
-          Authorization: "bearer " + process.env.REACT_APP_API_TOKEN
-        });
-        console.log(data)
-      } catch(err) {
-        console.log(err)
-      }
-    }
-    fetchData();
-  },[])
+	return (
+		<div className="FeaturedProducts">
+			<div className="top">
+				<h1>{type} product</h1>
+				<p>
+					lorem ipsum dolor sit amet, consecttuyur adsopiscing elit, sed do
+					eiusmod tempor incidid ut labre ret dlored mange aliqua. Quis ipsum
+					sespendisse il labore Risus. Coomod viverra maecsena accumsan lacus
+					fel facilisis labre et dolore magna aliqua quis. Ipsum ultrices
+					gravida. Risus commodo viverra maecenas.
+				</p>
+			</div>
+			<div className="bottom">
+				{data.map((item) => (
+					<Card item={item} key={item.id} />
+				))}
+			</div>
+		</div>
+	);
+};
 
-  return (
-    <div className='FeaturedProducts'>
-      <div className="top">
-        <h1>{type} product</h1>
-        <p>
-          lorem ipsum dolor sit amet, consecttuyur adsopiscing elit, sed do eiusmod tempor incidid ut labre ret dlored mange aliqua. Quis ipsum sespendisse il labore Risus. Coomod viverra maecsena accumsan lacus fel facilisis labre et dolore magna aliqua quis. Ipsum ultrices gravida. Risus commodo viverra maecenas.
-        </p>
-      </div>
-      <div className="bottom">
-        {data.map(item=>(
-          <Card item={item} key={item.id} />
-        ))}
-      </div>
-    </div>
-  )
-}
-
-export default FeaturedProducts
+export default FeaturedProducts;
