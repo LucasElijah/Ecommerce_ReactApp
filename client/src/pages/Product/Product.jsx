e30298de6a52c8b2e4ca2a6cc0d58fd4028b2bc3
LucasElijah <elijahlucas96@gmail.com>
2023-02-16 14:35:33 -0800

    welp

---
+++ client/src/pages/Product/Product.jsx
@@ -0,0 +1,101 @@
+import { AddShoppingCart, Balance, FavoriteBorder } from "@mui/icons-material";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import "./Product.scss";
+
+const Product = () => {
+	const id = useParams().id;
+	const [selectedImg, setSelectedImg] = useState("img");
+	const [quantity, setQuantity] = useState(1);
+
+	const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
+
+	return (
+		<div className="product">
+			{loading ? (
+				"loading"
+			) : (
+				<>
+					<div className="left">
+						<div className="images">
+							{/* Clicking on image makes it the primary on the page */}
+							<img
+								src={
+									process.env.REACT_APP_UPLOAD_URL +
+									data?.attributes?.img?.data?.attributes?.url
+								}
+								alt=""
+								onClick={(e) => setSelectedImg("img")}
+							/>
+							<img
+								src={
+									process.env.REACT_APP_UPLOAD_URL +
+									data?.attributes?.img2?.data?.attributes?.url
+								}
+								alt=""
+								onClick={(e) => setSelectedImg("img2")}
+							/>
+						</div>
+						<div className="mainImg">
+							<img
+								src={
+									process.env.REACT_APP_UPLOAD_URL +
+									data?.attributes[selectedImg]?.data?.attributes?.url
+								}
+								alt=""
+							/>
+						</div>
+					</div>
+
+					<div className="right">
+						<h1>{data?.attributes?.title}</h1>
+						<span className="price">${data?.attributes?.price}</span>
+						<p>
+							{data?.attributes?.desc}
+						</p>
+						<div className="quantity">
+							<button
+								onClick={() =>
+									setQuantity((prev) => (prev === 1 ? 1 : prev - 1))
+								}
+							>
+								-
+							</button>
+							{quantity}
+							<button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+						</div>
+						<button className="add">
+							<AddShoppingCart /> add to cart
+						</button>
+						<div className="links">
+							<div className="item">
+								<FavoriteBorder /> wishlist
+							</div>
+							<div className="item">
+								{" "}
+								compare
+								<Balance />
+							</div>
+						</div>
+						<div className="info">
+							<span>Vendor: _____</span>
+							<span>Product Type: _____</span>
+							<span>Tag: _____</span>
+						</div>
+						<hr />
+						<div className="info">
+							<span>DESCRIPTION</span>
+							<hr />
+							<span>ADDITIONAL INFO</span>
+							<hr />
+							<span>FAQ</span>
+						</div>
+					</div>
+				</>
+			)}
+		</div>
+	);
+};
+
+export default Product;
