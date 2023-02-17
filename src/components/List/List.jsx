163660aee5a6358a70395ee04cf5254829b5422b
LucasElijah <elijahlucas96@gmail.com>
2023-02-16 21:14:03 -0800

    k

---
+++ src/components/List/List.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./List.scss";
+import Card from "../Card/Card";
+
+const List = () => {
+
+	return (
+		<div className="list">
+			
+
+
+
+			{/*  {data?.map(item=>(
+      <Card item={item} key={item.id} />
+    ))} */}
+		</div>
+	);
+};
+
+export default List;
