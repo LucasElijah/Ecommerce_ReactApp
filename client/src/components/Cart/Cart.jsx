a6e8e86f750f724ae8c00983d67506a79635005d
LucasElijah <elijahlucas96@gmail.com>
2023-02-23 13:30:01 -0800

    cart works all of a sudden?
    still need to add function to delete from cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -6,13 +6,13 @@ import "./Cart.scss";
 const Cart = () => {
 	const products = useSelector((state) => state.cart.products);
 
-	/* const totalPrice = () => {
+	const totalPrice = () => {
 		let total = 0;
 		products.forEach((item) => {
 			total += item.quantity * item.price;
 		});
 		return total.toFixed(2);
-	}; */
+	};
 
 	return (
 		<div className="cart">
@@ -23,14 +23,14 @@ const Cart = () => {
 					<div className="details">
 						<h1>{item.title}</h1>
 						<p>{item.desc?.substring(0, 100)}</p>
-						<div className="price">1 x ${item.price}</div>
+						<div className="price">{item.quantity} x ${item.price}</div>
 					</div>
 					<DeleteOutlined className="delete" />
 				</div>
 			))}
 			<div className="total">
         <span>SUBTOTAL</span>
-        <span>${/* {totalPrice()} */}45454</span>
+        <span>${totalPrice()}</span>
       </div>
 			<button>Proceed to Checkout</button>
 			<span className="reset">Empty Cart</span>
